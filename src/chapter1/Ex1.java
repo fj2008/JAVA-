9046544d1e94a9eae7f6d01092d7a8e1b7433b9e
DESKTOP-NUOPUIF\ITPS <fj2008@naver.com>
2021-03-15 17:19:10 +0900

    최초 프로잭트 업로드

---
+++ src/chapter1/Ex1.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+//클래스: 소스코드의 최소 단위
+
+public class Ex1 {
+	//메인 함수 : 시작점, 내가 작성한 소스코드의 시작이 어딘지 알려주는 기능이라능
+	public static void main(String[] args) {
+		System.out.println("Hello world~!");
+		//주석 : 메모, 개발자가 참고해야할 사항
+		//System.out.println()은 무언가를 출력하는 기능
+		System.out.println("주석 연습중");
+		System.out.println("퍼킹 싯");
+		System.out.println("홀리싯");
+		System.out.println("자미온다~!");
+//		주석 단축키 ctrl+Shift+c
+		
+		
+	}
+
+}
---
+++ src/chapter1/Ex2.java
@@ -0,0 +1,34 @@
+package chapter1;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+//		System.out.println("Hello world~!");
+//	문자열은 꼭 쌍따옴표를 사용해서 표현한다.
+//		System.out.println("Hello");
+//		System.out.println("world~!");
+//		System.out.print("I am ");
+//		System.out.print("a ");
+//		System.out.print("java Programmer");
+//		println에서 ln을 입력하면서 c언어에\n기능을 담당하기때문에 줄바꿈이 되지 않는다
+		
+//		System.out.println(1);
+//		System.out.println(3.14);
+//		System.out.println('a');
+//		System.out.println("asdfq");
+		
+		System.out.print("오늘의 기온 :");
+		System.out.println(-5.3);
+		System.out.print("오늘의 기온 :");
+		System.out.println(1500);
+		System.out.print("오늘의 기온 :");
+		System.out.println('a');
+//		
+		
+		
+		
+		
+		
+		}
+
+}
